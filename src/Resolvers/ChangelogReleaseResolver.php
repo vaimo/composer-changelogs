6193da8d5b0c45dc57c430ce8f28367f425f3d0d
Allan Paiste <allan.paiste@vaimo.com>
2018-08-15 12:19:56 +0200

    fixes to the way branch filter works; allow special cases like 'default' and 'master' to match items that don't have branch set at all

--- src/Resolvers/ChangelogReleaseResolver.php
+++ src/Resolvers/ChangelogReleaseResolver.php
@@ -17,14 +17,14 @@ class ChangelogReleaseResolver
         $this->constraintValidator = new \Vaimo\ComposerChangelogs\Validators\ConstraintValidator();
     }
 
-    public function resolveUpcomingRelease(array $changelog, $branch = false)
+    public function resolveLatestVersionedRelease(array $changelog, $branch = false)
     {
         foreach ($changelog as $version => $item) {
-            if ($this->constraintValidator->isConstraint($version)) {
-                break;
+            if (!$this->constraintValidator->isConstraint($version)) {
+                continue;
             }
 
-            if (isset($item['branch']) && (!$branch || $item['branch'] !== $branch)) {
+            if (!$this->matchBranch($item, $branch)) {
                 continue;
             }
 
@@ -36,20 +36,37 @@ class ChangelogReleaseResolver
         return false;
     }
 
-    public function resolveLatestVersionedRelease(array $changelog, $branch = false)
+    public function resolveUpcomingRelease(array $changelog, $branch = false)
     {
         foreach ($changelog as $version => $item) {
-            if (!$this->constraintValidator->isConstraint($version)) {
-                continue;
+            if ($this->constraintValidator->isConstraint($version)) {
+                break;
             }
 
-            if (isset($item['branch']) && (!$branch || $item['branch'] !== $branch)) {
+            if (!$this->matchBranch($item, $branch)) {
                 continue;
             }
 
             return $version;
 
             break;
+        }
+
+        return false;
+    }
+
+    private function matchBranch(array $item, $branch)
+    {
+        if (!isset($item['branch']) && !$branch) {
+            return true;
+        }
+
+        if (!isset($item['branch']) && ($branch === 'master' || $branch === 'default')) {
+            return true;
+        }
+
+        if (isset($item['branch']) && $item['branch'] === $branch) {
+            return true;
         }
 
         return false;
